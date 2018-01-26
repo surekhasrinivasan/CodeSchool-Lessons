58649d21f31e4e30d8e3e99e818cbbbe13867676
surekha <surekhasrao@gmail.com>
2018-01-26 02:58:53 +0000

    CodeSchool Express Level-4 Part-2

--- ExpressLessons/level-4-challenges.js
+++ ExpressLessons/level-4-challenges.js
@@ -58,3 +58,57 @@ app.post('/cities', parseUrlencoded, function (request, response) {
     }
 });
 app.listen(3000);
+
+//3. Response Body 
+
+//What would the response body be set to on a DELETE request to /cities/DoesNotExist ? 
+//Here's the link to the sendStatus function source code if you need to take a look.
+//200
+// Answer: 404
+
+// 4. Delete Route
+
+//Create a Dynamic Route for deleting cities and handle for cities that are not in our list.
+// app.js
+var express = require('express');
+var app = express();
+
+var cities = {
+  'Lotopia': 'Rough and mountainous',
+  'Caspiana': 'Sky-top island',
+  'Indigo': 'Vibrant and thriving',
+  'Paradise': 'Lush, green plantation',
+  'Flotilla': 'Bustling urban oasis'
+};
+
+app.param('name', function (request, response, next) {
+  request.cityName = parseCityName(request.params.name);
+});
+// a. Create a DELETE route that takes the city name as its first argument, 
+//followed by a callback that takes a request and response objects as arguments.
+app.delete('/cities/:name', function(request, response){
+    //d. Add an if block that checks whether the cityName provided from app.param() 
+    //has a valid entry before attempting to delete it from the cities object. 
+    //If a valid city is not found, then respond with a 404 HTTP status code using 
+    //the sendStatus() function.
+    if(cities[request.cityName]){
+    // b. Use the built-in JavaScript operator delete (see MDN docs) to remove the property 
+    //for the city passed as an argument. Don't forget to use the attribute set in app.param() 
+    //to look the city up.
+    delete cities[request.cityName];
+    // c. Use sendStatus() to respond back with a status code of 200.
+    response.sendStatus(200);
+    }
+    // e. Add an if block that checks whether the cityName provided from app.param() has 
+    //a valid entry before attempting to delete it from the cities object. If a valid city 
+    //is not found, then respond with a 404 HTTP status code using the sendStatus() function.
+    else{
+        response.sendStatus(404);
+    }
+});      
+app.listen(3000);
+
+function parseCityName(name) {
+  var parsedName = name[0].toUpperCase() + name.slice(1).toLowerCase();
+  return parsedName;
+}
