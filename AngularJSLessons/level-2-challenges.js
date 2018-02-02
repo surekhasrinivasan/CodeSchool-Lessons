2b67f5dbd9810a89de693ae8fdbdb4a6310ed21d
surekha <surekhasrao@gmail.com>
2018-02-02 19:29:04 +0000

    CodeSchool Angular.js Level-2-part-2

--- AngularJSLessons/level-2-challenges.js
+++ AngularJSLessons/level-2-challenges.js
@@ -180,3 +180,370 @@
   </body>
 </html>
 
+// 5. Tabs Inside Out 
+
+//We just learned that cramming a bunch of logic into our HTML is kinda bad. 
+//Let's start the Right Way with a Controller which will control our tabs.
+
+//app.js
+(function() {
+  var app = angular.module('gemStore', []);
+
+  app.controller('StoreController', function(){
+    this.products = gems;
+  });
+  // a. Create a controller named TabController.
+    app.controller('TabController', function(){
+        // b. An empty Controller isn't much use. Do you remember why we needed a Controller 
+        //at all? That's right, we need to initialize the tab property. Go ahead and 
+        //add that property now, setting it to 1.
+        
+        this.tab = 1;
+        
+        //c. In order to set the current tab, we'll need a setTab method to use in our HTML. 
+        //It should set the tab property of TabController to a value passed in.
+        
+        this.setTab = function(selectedTab){
+            this.tab = selectedTab;
+        };
+        
+        // d. We've got a setTab method. Now we need an isSet method that accepts a value 
+        //and returns whether that value matches this.tab.
+        
+        his.isSet = function(checkTab){
+            return this.tab === checkTab; 
+        };
+    });
+  
+
+  var gems = [
+      {
+        name: 'Azurite',
+        description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+        shine: 8,
+        price: 110.50,
+        rarity: 7,
+        color: '#CCC',
+        faces: 14,
+        images: [
+          "images/gem-02.gif",
+          "images/gem-05.gif",
+          "images/gem-09.gif"
+        ],
+        reviews: [{
+          stars: 5,
+          body: "I love this gem!",
+          author: "joe@example.org",
+          createdOn: 1397490980837
+        }, {
+          stars: 1,
+          body: "This gem sucks.",
+          author: "tim@example.org",
+          createdOn: 1397490980837
+        }]
+      },
+      {
+        name: 'Bloodstone',
+        description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+        shine: 9,
+        price: 22.90,
+        rarity: 6,
+        color: '#EEE',
+        faces: 12,
+        images: [
+          "images/gem-01.gif",
+          "images/gem-03.gif",
+          "images/gem-04.gif",
+        ],
+        reviews: [{
+          stars: 3,
+          body: "I think this gem was just OK, could honestly use more shine, IMO.",
+          author: "JimmyDean@example.org",
+          createdOn: 1397490980837
+        }, {
+          stars: 4,
+          body: "Any gem with 12 faces is for me!",
+          author: "gemsRock@example.org",
+          createdOn: 1397490980837
+        }]
+      },
+      {
+        name: 'Zircon',
+        description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+        shine: 70,
+        price: 1100,
+        rarity: 2,
+        color: '#000',
+        faces: 6,
+        images: [
+          "images/gem-06.gif",
+          "images/gem-07.gif",
+          "images/gem-09.gif"
+        ],
+        reviews: [{
+          stars: 1,
+          body: "This gem is WAY too expensive for its rarity value.",
+          author: "turtleguyy@example.org",
+          createdOn: 1397490980837
+        }, {
+          stars: 1,
+          body: "BBW: High Shine != High Quality.",
+          author: "LouisW407@example.org",
+          createdOn: 1397490980837
+        }, {
+          stars: 1,
+          body: "Don't waste your rubles!",
+          author: "nat@example.org",
+          createdOn: 1397490980837
+        }]
+      }
+    ];
+})();
+
+// 6. Using TabController 
+
+//To connect our controller to our page, we're going to need a few directives. Try to figure
+//out which ones to use, but if you get stuck we'll give you hints along the way. 
+//We've provided all the HTML for you. Try to accomplish the following tasks:
+
+<!DOCTYPE html>
+<html ng-app="gemStore">
+  <head>
+    <link rel="stylesheet" type="text/css" href="bootstrap.min.css" />
+    <script type="text/javascript" src="angular.min.js"></script>
+    <script type="text/javascript" src="app.js"></script>
+  </head>
+  <body class="list-group" ng-controller="StoreController as store">
+    <header>
+      <h1 class="text-center">Flatlander Crafted Gems</h1>
+      <h2 class="text-center">– an Angular store –</h2>
+    </header>
+    <div class="list-group-item" ng-repeat="product in store.products">
+      <h3>
+        {{product.name}}
+        <em class="pull-right">{{product.price | currency}}</em>
+      </h3>
+      <section ng-show="product.images.length">
+        <img ng-src="{{product.images[0]}}" />
+        <ul class="list-inline thumbs">
+          <li class="thumbnail" ng-repeat="image in product.images">
+            <img ng-src="{{image}}" />
+          </li>
+        </ul>
+      </section>
+
+// a. Attach the TabController to the <section> element with the .tab class. 
+//Don't forget to also add an alias.
+        <section class="tab" ng-controller="Tabontroller as tab">
+        <ul class="nav nav-pills">
+            // b. Trigger the setTab method when a link inside a tab is clicked. 
+            //Add this to each of the tab links. Pass in the number of the tab, in this case 1, 2 or 3.
+          //d. Add the active class to the correct <li> when each tab is clicked. You'll need to 
+          //add a directive to each <li> in .nav.
+          <li ng-class="{active: tab.isSet(1)}">
+            <a href ng-click="tab.setTab(1)">Description</a></li>
+          <li ng-class="{active: tab.isSet(2)}">
+            <a href ng-click="tab.setTab(2)">Specs</a></li>
+          <li ng-class="{active: tab.isSet(3)}">
+            <a href ng-click="tab.setTab(3)">Reviews</a></li>
+        </ul>
+        // c. Use the isSet method to show the corresponding tab. You'll need to add 
+        //a directive to each of the <div> elements in .tab.
+        <div ng-show="tab.isSet(1)">
+          <h4>Description</h4>
+         // e. Within the Description tab, output the product's description within the <blockquote> element.
+          <blockquote>{{product.description}}</blockquote>
+        </div>
+        <div ng-show ="tab.isSet(2)">
+          <h4>Specs</h4>
+          // f. Within the Specs tab, output the product's shine value. 
+          //It should look something like this within the <blockquote> element: "Shine: 4".
+          <blockquote>Shine: {{product.shine}}</blockquote>
+        </div>
+        <div ng-show ="tab.isSet(3)">
+          <h4>Reviews</h4>
+          <blockquote></blockquote>
+        </div>
+      </section>
+    </div>
+  </body>
+</html>
+
+// 7. Creating Gallery Controller 
+
+//Now that you're such a whiz with tabbed tab, can you apply what you've learned to
+//our image gallery, too? Don't worry, we'll start slow… And there's a 
+//lot more in common than you might think!
+
+//app.js
+(function() {
+  var app = angular.module('gemStore', []);
+// a. Let's start with a Controller, just like last time. Call it GalleryController.
+  app.controller('GalleryController', function(){
+    // b. Initialize a current property of GalleryController with a value of 0.
+    this.current = 0;
+    // c. Add a method to GalleryController called setCurrent that accepts a 
+    //value and assigns it to current. If no value is passed in, set current to 0.
+    this.setCurrent = function(newGallery){
+        this.current = newGallery || 0;
+    };
+});  
+  app.controller('StoreController', function(){
+    this.products = gems;
+  });
+
+  app.controller('TabController', function(){
+    this.tab = 1;
+
+    this.setTab = function(newValue){
+      this.tab = newValue;
+    };
+
+    this.isSet = function(tabName){
+      return this.tab === tabName;
+    };
+  });
+
+  var gems = [
+    {
+      name: 'Azurite',
+      description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+      shine: 8,
+      price: 110.50,
+      rarity: 7,
+      color: '#CCC',
+      faces: 14,
+      images: [
+        "images/gem-02.gif",
+        "images/gem-05.gif",
+        "images/gem-09.gif"
+      ],
+      reviews: [{
+        stars: 5,
+        body: "I love this gem!",
+        author: "joe@example.org",
+        createdOn: 1397490980837
+      }, {
+        stars: 1,
+        body: "This gem sucks.",
+        author: "tim@example.org",
+        createdOn: 1397490980837
+      }]
+    },
+    {
+      name: 'Bloodstone',
+      description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+      shine: 9,
+      price: 22.90,
+      rarity: 6,
+      color: '#EEE',
+      faces: 12,
+      images: [
+        "images/gem-01.gif",
+        "images/gem-03.gif",
+        "images/gem-04.gif",
+      ],
+      reviews: [{
+        stars: 3,
+        body: "I think this gem was just OK, could honestly use more shine, IMO.",
+        author: "JimmyDean@example.org",
+        createdOn: 1397490980837
+      }, {
+        stars: 4,
+        body: "Any gem with 12 faces is for me!",
+        author: "gemsRock@example.org",
+        createdOn: 1397490980837
+      }]
+    },
+    {
+      name: 'Zircon',
+      description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+      shine: 70,
+      price: 1100,
+      rarity: 2,
+      color: '#000',
+      faces: 6,
+      images: [
+        "images/gem-06.gif",
+        "images/gem-07.gif",
+        "images/gem-09.gif"
+      ],
+      reviews: [{
+        stars: 1,
+        body: "This gem is WAY too expensive for its rarity value.",
+        author: "turtleguyy@example.org",
+        createdOn: 1397490980837
+      }, {
+        stars: 1,
+        body: "BBW: High Shine != High Quality.",
+        author: "LouisW407@example.org",
+        createdOn: 1397490980837
+      }, {
+        stars: 1,
+        body: "Don't waste your rubles!",
+        author: "nat@example.org",
+        createdOn: 1397490980837
+      }]
+    }
+  ];
+})();
+
+// 8. Using Gallery Controller
+
+//Now that we've got an awesome GalleryController, let's wire it up to the page:
+// index.html
+<!DOCTYPE html>
+<html ng-app="gemStore">
+  <head>
+    <link rel="stylesheet" type="text/css" href="bootstrap.min.css" />
+    <script type="text/javascript" src="angular.min.js"></script>
+    <script type="text/javascript" src="app.js"></script>
+  </head>
+  <body class="list-group" ng-controller="StoreController as store">
+    <header>
+      <h1 class="text-center">Flatlander Crafted Gems</h1>
+      <h2 class="text-center">– an Angular store –</h2>
+    </header>
+    <div class="list-group-item" ng-repeat="product in store.products">
+      <h3>
+        {{product.name}}
+        <em class="pull-right">{{product.price | currency}}</em>
+      </h3>
+
+      <!-- Image Gallery  -->
+      // a. Attach GalleryController to the .gallery element that wraps our gallery; use the alias gallery.
+      <div class='gallery'ng-controller = "GalleryController as gallery" ng-show="product.images.length">
+        // b. Change the first ng-src directive to use the current property. Properly use current as the index instead of 0.
+        <!--<img ng-src="{{product.images[0]}}" />-->
+        <img ng-src="{{product.images[gallery.current]}}" />
+        <ul class="list-inline thumbs">
+          <li class="thumbnail" ng-repeat="image in product.images">
+            <img ng-src="{{image}}" />
+          </li>
+        </ul>
+      </div>
+
+      <section class="tab" ng-controller="TabController as tab">
+        <ul class="nav nav-pills">
+          <li ng-class="{ active: tab.isSet(1) }">
+            <a href ng-click="tab.setTab(1)">Description</a></li>
+          <li ng-class="{ active: tab.isSet(2) }">
+            <a href ng-click="tab.setTab(2)">Specs</a></li>
+          <li ng-class="{ active: tab.isSet(3) }">
+            <a href ng-click="tab.setTab(3)">Reviews</a></li>
+        </ul>
+        <div ng-show="tab.isSet(1)">
+          <h4>Description</h4>
+          <blockquote>{{product.description}}</blockquote>
+        </div>
+        <div ng-show="tab.isSet(2)">
+          <h4>Specs</h4>
+          <blockquote>Shine: {{product.shine}}</blockquote>
+        </div>
+        <div ng-show="tab.isSet(3)">
+          <h4>Reviews</h4>
+        </div>
+      </section>
+    </div>
+  </body>
+</html>
