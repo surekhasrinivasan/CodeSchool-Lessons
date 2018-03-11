49e6fd2ee20d0089390f743e596f6dfb4bbd1216
surekha <surekhasrao@gmail.com>
2018-03-11 20:40:26 +0000

    CodeSchool MongoDB Level-4-part-1

---
+++ MongoDBLessons/level-4-challenges.js
@@ -0,0 +1,23 @@
+// 1. Marvelous Merging 
+//Fill in the blank with the correct answer.
+
+//When we take the data from one document and place it inside another one, that's called an 
+// embedded document.
+
+//2. Related Realms 
+//Fill in the blank with the correct answer.
+
+// If we take a unique value like an _id from one document and store it as a value 
+//within a related document, we have just created a referenced document.
+
+//3. Lookout Incantations 
+
+// For what reason might we want to consider referencing maker information instead of 
+//embedding it within each wand?
+
+// Atomic Writes
+
+// Answer: Duplicate Data
+
+// Single Query Access
+
