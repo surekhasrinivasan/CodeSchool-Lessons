1f550986563c5b2e3dcef6803b57891b16e4c24a
surekha <surekhasrao@gmail.com>
2018-03-12 20:03:22 +0000

    CodeSchool MongoDB Level-4-part-2

--- MongoDBLessons/level-4-challenges.js
+++ MongoDBLessons/level-4-challenges.js
@@ -45,3 +45,45 @@
 // Embedded
 
 // Answer: Referenced
+
+//7.  Casting Choices
+//In general, what's the best option to first consider for modeling your related data?
+
+//Answer: Embedding
+
+//Referencing
+
+
+//8. Unique Users 250 PTS
+//Which data modeling option would be the best fit for storing users and their addresses 
+//when we know that the data is used together often, won't be changing regularly, and 
+//each user will only be storing a few addresses?
+
+//Answer: Embedding
+
+// Referencing
+
+//9. Charming Cars
+//We'd like to store information about cars, and each car can have a few hundred parts. 
+//Most of the time, we won't be needing specific information about each part. 
+//Which data modeling route should we take?
+
+// Embedding
+
+//Answer: Referencing
+
+//10. Charming Changes 
+//Which modeling route is best when we have data that is constantly changing and will 
+//help prevent data inconsistencies from arising?
+
+// Embedding
+
+//Answer: Referencing
+
+//11. Bewitched Access 
+// Which data modeling route allows us to access our data independently instead of 
+//having to use something like dot notation to get information?
+
+// Embedding
+
+//Answer: Referencing
