635af3d25d0764df1632d55ed75bd0b669951af9
surekha <surekhasrao@gmail.com>
2018-03-14 19:38:30 +0000

    CodeSchool MongoDB Level-5-part-2

--- MongoDBLessons/level-5-challenges.js
+++ MongoDBLessons/level-5-challenges.js
@@ -72,3 +72,93 @@ db.wands.aggregate([{"$group":{
                                 "max_magic":{"$max":"$damage.magic"},
                                 "lowest_price":{"$min":"$price"}
 }}]);
+
+//6. Lower-level Castings 
+//Some wand powers can be harder to find in lower-level wands. We've heard that the power
+//"Air Bolt" is a really fun one to have. Let's find out which makers offer a wand with that 
+//power and find the lowest level_required per maker.
+
+//a. Write an aggregate that will only match wands that have "Air Bolt" in their list of powers.
+
+db.wands.aggregate([{"$match":{"powers":"Air Bolt"}}])
+
+//b. Add another aggregate stage to group the data by their maker.
+
+db.wands.aggregate([{"$match":{"powers":"Air Bolt"}},
+                   {"$group":{"_id":"$maker"}}])
+                   
+//c. Add an accumulator with a field named lowest_level that finds the lowest level_required per maker.                   
+
+db.wands.aggregate([{"$match":{"powers":"Air Bolt"}},
+                   {"$group":{"_id":"$maker", "lowest_level":{"$min":"$level_required"}}}])
+
+//7. Budget Castings 
+//A user has asked us to find out which makers have wands that are under 50 gems and 
+//have a damage.magic average above 40.
+
+//a. Write an aggregate to match wands that have a price that is less than 50.
+
+db.wands.aggregate([{"$match":{"price":{"$lt":50}}}])
+
+//b. Add the aggregate stage to group the wands by their maker.
+
+db.wands.aggregate([{"$match":{"price":{"$lt":50}}},
+                   {"$group":{"_id":"$maker"}}])
+
+//c. Add an accumulator with a field named average_magic to find the average damage.magic 
+//per maker using the $avg accumulator.                   
+
+db.wands.aggregate([{"$match":{"price":{"$lt":50}}},
+                   {"$group":{"_id":"$maker", "average_magic":{"$avg":"$damage.magic"}}}])
+
+//d. Now that we've got our data all set, let's filter it down further.
+
+//Below the existing $match and $group aggregates, add another $match aggregate so that 
+//we're only retrieving results with an average_magic that is greater than 40.
+
+db.wands.aggregate([{"$match":{"price":{"$lt":50}}},
+                   {"$group":{"_id":"$maker", "average_magic":{"$avg":"$damage.magic"}}},
+                   {"$match":{"average_magic":{"$gt":40}}}])
+
+//8. Clairvoyant Decisions
+//We're always on the lookout for the best wand for its value. Let's find out the top 5 makers 
+//that offer the most magic damage for a wand in our level range.
+
+//a. Write an aggregate that finds wands that have a level_required that's less than or equal to 5.
+
+db.wands.aggregate([{"$match":{"level_required":{"$lte":5}}}])
+
+//b. Add the aggregate stage to group the wands by their maker.
+
+db.wands.aggregate([{"$match":{"level_required":{"$lte":5}}},
+                   {"$group":{"_id":"$maker"}}])
+
+//c. Add an accumulator with the field max_damage that finds the max damage.magic per maker.                   
+
+db.wands.aggregate([{"$match":{"level_required":{"$lte":5}}},
+                   {"$group":{"_id":"$maker", "max_damage":{"$max":"$damage.magic"}}}])                   
+
+//d. Now that we have the bulk of our data, let's go ahead and sort the max_damage in descending order.
+
+db.wands.aggregate([{"$match":{"level_required":{"$lte":5}}},
+                   {"$group":{"_id":"$maker", "max_damage":{"$max":"$damage.magic"}}},
+                   {"$sort":{"max_damage":-1}}]);
+
+//e. Add a limit stage so that we only find the first 4 results. After all, we don't have all day to look through wands!
+
+db.wands.aggregate([{"$match":{"level_required":{"$lte":5}}},
+                   {"$group":{"_id":"$maker", "max_damage":{"$max":"$damage.magic"}}},
+                   {"$sort":{"max_damage":-1}},
+                   {"$limit": 4}]);
+
+//f. There's one more stage we can add to our pipeline to make sure it's fully optimized. 
+//Since we only need the maker and damage.magic fields, add a $project stage that only passes 
+//those fields along to the rest of operators.
+
+//Remember, where you place this is important!
+                   
+db.wands.aggregate([{"$match":{"level_required":{"$lte":5}}},
+                    {"$project":{"_id":false,"maker":true, "damage.magic":true}},
+                   {"$group":{"_id":"$maker", "max_damage":{"$max":"$damage.magic"}}},
+                   {"$sort":{"max_damage":-1}},
+                   {"$limit": 4}]); 
